c9f133d7fa4dea977ba6174a035e8557883d9e6f
chrisfender0 <chrisjuste@localhost>
2020-02-23 08:53:08 -0800

    setup graph outline

--- FfunctionBuilder/src/alpha/Graph.java
+++ FfunctionBuilder/src/alpha/Graph.java
@@ -1,13 +1,39 @@
 package alpha;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
 
 public class Graph extends JPanel {
 	
-	private Graph() {
-		
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		paintGraphOutline(g);
+		draw(g);
 	}
 	
+	private void paintGraphOutline(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setColor(Color.black);
+		g2d.setStroke(new BasicStroke(2));
+		g2d.drawLine(getWidth()/2, 0, getWidth()/2, getHeight()); //y axis 
+		g2d.drawLine(0, getHeight()/2, getWidth(), getHeight()/2);
+	}
 	
+	private void draw(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		Graphics2D lastPoint = (Graphics2D) g;
+		int x = getWidth()/2;
+		int y = getHeight()/2; 
+		
+		g2d.setColor(Color.blue);
+		g2d.drawLine(x, y, x+100, y-100);
+		g2d.drawLine(x+100, y-100, x+150, y-40);
+	}
 
 }
--- FfunctionBuilder/src/alpha/MainWindow.java
+++ FfunctionBuilder/src/alpha/MainWindow.java
@@ -1,11 +1,34 @@
 package alpha;
 
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 
 public class MainWindow extends JFrame{
+	
+	
 
 	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
 
+            	MainWindow ex = new MainWindow();
+                ex.setVisible(true);
+            }
+        });
+	}
+	
+	private MainWindow() {
+		init();
+	}
+	
+	private void init() {
+		Graph graph = new Graph();
+		add(graph);
+		setTitle("Graph");
+		setSize(1000, 1000);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
